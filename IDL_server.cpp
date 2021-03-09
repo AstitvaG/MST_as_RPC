0a22b9e2532d7865baf5f082365bb89119a05e3b
Astitva Gupta <gupta.astitva@gmail.com>
2021-03-10 00:02:24 +0530

    Corrected server file

--- IDL_server.cpp
+++ IDL_server.cpp
@@ -2,7 +2,7 @@
 #include "IDL.h"
 using namespace std;
 
-typedef set<pair<int, pair<int, int>>> my_set;
+typedef set<vector<int>> my_set;
 
 struct graph
 {
@@ -11,101 +11,106 @@ struct graph
 	vector<int> arr;
 	vector<int> weight;
 };
-long long sum = 0;
-
-int n;
-my_set kset;
-int arr[100001];
-int weight[100001];
 
 map<string, graph> graphs;
 
-int find(int src)
+int find(int src, graph &G)
 {
 	int hold = src;
-	while (arr[src] != -1)
+	while (G.arr[src] != -1)
 	{
-		src = arr[src];
+		src = G.arr[src];
 	}
 	int temp;
-	while (arr[hold] != -1)
+	while (G.arr[hold] != -1)
 	{
-		temp = arr[hold];
-		arr[hold] = src;
+		temp = G.arr[hold];
+		G.arr[hold] = src;
 		hold = temp;
 	}
 	return src;
 }
 
-void uni(int x, int y)
+void uni(int x, int y, graph &G)
 {
-	int rx = find(x);
-	int ry = find(y);
+	int rx = find(x, G);
+	int ry = find(y, G);
 	if (rx == ry)
 		return;
-	if (weight[rx] >= weight[ry])
+	if (G.weight[rx] >= G.weight[ry])
 	{
-		arr[ry] = rx;
-		weight[rx] += weight[ry];
+		G.arr[ry] = rx;
+		G.weight[rx] += G.weight[ry];
 	}
 	else
 	{
-		arr[rx] = ry;
-		weight[ry] += weight[ry];
+		G.arr[rx] = ry;
+		G.weight[ry] += G.weight[ry];
 	}
 }
 
-void kruskals()
+long kruskals(graph &G)
 {
-	for (int i = 0; i < n - 1; i++)
+	long sum = 0, number_edges = 0;
+	G.arr = vector<int>(G.n, -1);
+	G.weight = vector<int>(G.n, -1);
+	auto kset = G.kset;
+	vector<vector<int>> res;
+	int size = kset.size();
+	for (int i = 0; i < size; i++)
 	{
-		int wt = (*kset.begin()).first;
-		int u = (*kset.begin()).second.first;
-		int v = (*kset.begin()).second.second;
+		int wt = (*kset.begin())[0];
+		int u = (*kset.begin())[1];
+		int v = (*kset.begin())[2];
 		kset.erase(kset.begin());
 		cout << wt << "=weight " << u << "=n1 " << v << "=n2 ";
-		int ux = find(u);
-		int vx = find(v);
+		int ux = find(u, G);
+		int vx = find(v, G);
 		cout << ux << "=f1 " << vx << "=f2\n";
 		if (ux != vx)
 		{
-			uni(u, v);
+			uni(u, v, G);
 			sum += wt;
+			number_edges++;
+			res.push_back({u, v, wt});
 		}
 	}
+	for (auto j : res)
+	{
+		for (auto k : j)
+			cout << k << " ";
+		cout << endl;
+	}
+	return number_edges < G.n - 1 ? -1 : sum;
 }
 
 void *add_graph_6_svc(mpc_struct *argp, struct svc_req *rqstp)
 {
 	static char *result;
-	printf("add graph called\n");
-	/*
-	 * insert server code here
-	 */
-
+	cout << "Adding graph.." << endl;
+	int n = argp->args[0];
+	graphs[argp->graph_id] = {
+		n,
+		my_set(),
+		vector<int>(n, -1),
+		vector<int>(n, 1)};
+	cout << "Graph added!" << endl;
 	return (void *)&result;
 }
 
 void *add_egde_6_svc(mpc_struct *argp, struct svc_req *rqstp)
 {
 	static char *result;
-
-	printf("add edge called\n");
-	/*
-	 * insert server code here
-	 */
-
+	cout << "Adding edge.." << endl;
+	graphs[argp->graph_id].kset.insert({argp->args[2], argp->args[0], argp->args[1]});
+	cout << "Edge added!" << endl;
 	return (void *)&result;
 }
 
 long *get_mst_6_svc(mpc_struct *argp, struct svc_req *rqstp)
 {
 	static long result;
-
-	printf("get mst called\n");
-	/*
-	 * insert server code here
-	 */
-
+	cout << "Getting MST" << endl;
+	result = kruskals(graphs[argp->graph_id]);
 	return &result;
 }
